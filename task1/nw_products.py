166853125643c7bb65b24a12b2f1354d0de07a0f
jared <jtlss14@gmail.com>
2020-11-11 22:43:28 +0000

    finished iteration

--- task1/nw_products.py
+++ task1/nw_products.py
@@ -2,14 +2,29 @@ import pyodbc
 from task import SQLInstance
 
 server = "JaredPC\JS_1"
-database = "TASK"
+database = "Northwind"
 username = "sa"
 password = "passw0rd"
 
+# This class will inherit from the SQLInstance class
+# This allows us to re-use the code to connect to the server automatically
 class Products(SQLInstance):
     def __init__(self, server, database, username, password):
         super().__init__(server, database, username, password)
 
+    # Returns the average UnitPrice
+    def print_average(self):
+        query = "SELECT AVG(UnitPrice) FROM Products"
+        avg = self.cursor.execute(query).fetchone()
+        print(f"\nThe average UnitPrice is {avg[0]}")
 
-c = Products(server, database, username, password)
-print(c.cursor)
+
+    # Shows the column names in the Products DB
+    def show_col(self):
+        super().show_col_names("Products")
+
+
+# Runs if executing file and not importing it
+if __name__ == "__main__":
+    c = Products(server, database, username, password)
+    c.print_average()
